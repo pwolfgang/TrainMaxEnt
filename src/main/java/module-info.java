b380b2f146abdcdc29f93dabba01c2c73ddec337
Paul Wolfgang <wolfgang@temple.edu>
2019-02-18 13:58:55 -0500

    Updated pom and module-info.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module edu.temple.cla.papolicy.texttools.TrainMaxent {
+    requires edu.temple.cla.papolicy.wolfgang.TextToolsUtil;
+    requires edu.stanford.nlp.StanfordClassifier;
+    exports edu.temple.cla.papolicy.wolfgang.texttools.trainmaxent;
+}
